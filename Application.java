c7c107d5138c1959db0a5d23fdd78eb33b4178da
Demotylerk <60150496+Demotylerk@users.noreply.github.com>
2020-01-25 18:36:12 -0500

    Update Application.java

--- Application.java
+++ Application.java
@@ -2,6 +2,8 @@ import java.math.BigDecimal;
 
 public class Application implements Runnable{
 
+	//Violation #1
+	//https://wiki.sei.cmu.edu/confluence/display/java/NUM10-J.+Do+not+construct+BigDecimal+objects+from+floating-point+literals
 	public static BigDecimal num = new BigDecimal(1.0);
 	public static Logger logger = new Logger();
 
@@ -21,13 +23,13 @@ public class Application implements Runnable{
 	//Each thread adds to the running total, and pushes the result to the log
 	@Override
 	public void run() {
+		//Violation 2
+		//https://wiki.sei.cmu.edu/confluence/display/java/NUM07-J.+Do+not+attempt+comparisons+with+NaN
 		if(Double.parseDouble(num.toString()) == Double.NaN) {
 			logger.addLog("Invalid Number!");
 		} else {
 			logger.addLog(num.toString());
 		}
 	}
-
-
 }
 
